33ef15a02b9f27cdcdded698a01a095a9eda9714
Chris Akring <ddflj3210@gmail.com>
2019-08-01 14:52:37 +0800

    Add os version limit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "ImageTag",
+    platforms: [
+        // specify each minimum deployment requirement,
+        //otherwise the platform default minimum is used.
+        .iOS(.v10),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
